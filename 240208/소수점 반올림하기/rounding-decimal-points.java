fff9bd5b64658d3242947b865e4fd3305b739670
bnjjong <44669620+bnjjong@users.noreply.github.com>
2024-02-08 00:53:02 +0900

    Title: 소수점 반올림하기, Time: 91ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240208/소수점 반올림하기/rounding-decimal-points.java
@@ -0,0 +1,8 @@
+import java.lang.Math;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        double a = Math.round(25.352d * 10) / 10.0;
+        System.out.printf("%.1f", a);
+    }
+}
