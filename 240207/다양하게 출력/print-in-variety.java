d5e4f31c9ce37526796b7e984c6ce60cd0858ca0
bnjjong <44669620+bnjjong@users.noreply.github.com>
2024-02-07 17:25:44 +0900

    Title: 다양하게 출력, Time: 95ms, Memory: 8MB, Status: Passed - CodeTree

--- 240207/다양하게 출력/print-in-variety.java
+++ 240207/다양하게 출력/print-in-variety.java
@@ -4,6 +4,6 @@ public class Main {
         System.out.println("Total days in Year");
         System.out.println(365);
         System.out.println("Circumference rate");
-        System.out.println(3.1415926535f);
+        System.out.println(3.1415926535d);
     }
 }
