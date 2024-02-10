5cc19ef0f0b8a21e3c6cfccd177b598db6c6d863
bnjjong <44669620+bnjjong@users.noreply.github.com>
2024-02-10 21:54:20 +0900

    Title: 괄호 쌍 만들어주기 3, Time: 136ms, Memory: 10MB, Status: Passed - CodeTree

--- 240210/괄호 쌍 만들어주기 3/pair-parentheses-3.java
+++ 240210/괄호 쌍 만들어주기 3/pair-parentheses-3.java
@@ -8,6 +8,8 @@ public class Main {
         // 1. '(' 를 찾는다. 
         // 2. 해당 위치에서 그 뒤의 ')' 나온것 만큼 더한다.
         // 3. 반복해서 '(' 를 찾는다.
+        // 다만 좋은 로직은 아니다. 완전 탐색이라 무식하게 해결 함.
+        // O(N^2)이 아닌 O(N)으로 처리 가능할 듯.
         int resultSum = 0;
         for(int i=0; i<inputSize; i++) {
             char s = input.charAt(i);
