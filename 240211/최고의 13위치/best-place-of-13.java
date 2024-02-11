d7593cf37ad96ec8b4ec069786df41a293638e82
bnjjong <44669620+bnjjong@users.noreply.github.com>
2024-02-11 22:11:24 +0900

    Title: 최고의 13위치, Time: 240ms, Memory: 10MB, Status: Passed - CodeTree

--- 240211/최고의 13위치/best-place-of-13.java
+++ 240211/최고의 13위치/best-place-of-13.java
@@ -8,14 +8,17 @@ public class Main {
         int n = Integer.parseInt(sc.nextLine());
         // 2 차원 배열 선언 및 내용 채우기
         int[][] grid = new int[n][n];
-        for (int i=0; i<n; i++) {
-            int[] row = Arrays.stream(sc.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-            for (int j=0; j<row.length; j++) {
-                grid[i][j] = row[j];
-                // System.out.print(grid[i][j]);
-            }
-            // System.out.println("");
-        }
+        // for (int i=0; i<n; i++) {
+        //     int[] row = Arrays.stream(sc.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        //     for (int j=0; j<row.length; j++) {
+        //         grid[i][j] = row[j];
+        //         // System.out.print(grid[i][j]);
+        //     }
+        //     // System.out.println("");
+        // }
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < n; j++)
+                grid[i][j] = sc.nextInt();
 
         // 1 * 3 최대 동전 수 구하기
         int coinMax = 0;
@@ -24,6 +27,7 @@ public class Main {
             for (int j=0; j<n-2; j++) {
                 coinMax = Math.max(coinMax, grid[i][j]+grid[i][j+1]+grid[i][j+2]);
             }
+            // 어짜피 max 값이 나오면 더 연산할 필요가 없다.
             if (coinMax == maxInGrid) {
                 break;
             }
