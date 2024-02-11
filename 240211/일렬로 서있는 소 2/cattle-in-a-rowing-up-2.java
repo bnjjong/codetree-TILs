5c0241028b79a9ffee9b199d2d49e648a8a7bdc7
bnjjong <44669620+bnjjong@users.noreply.github.com>
2024-02-11 21:51:07 +0900

    Title: 일렬로 서있는 소 2, Time: 288ms, Memory: 15MB, Status: Passed - CodeTree

--- 240211/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.java
+++ 240211/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.java
@@ -30,6 +30,13 @@ public class Main {
             }
         }
 
+        // 해설
+        // for(int i = 0; i < n; i++)
+        //     for(int j = i + 1; j < n; j++)
+        //         for(int k = j + 1; k < n; k++)
+        //             if(arr[i] <= arr[j] && arr[j] <= arr[k])
+        //                 cnt++;
+
         System.out.print(resultCnt);
 
     }
