9083f4e5bf37fca7848528e7ade9a72c818895ba
bnjjong <44669620+bnjjong@users.noreply.github.com>
2024-02-15 14:40:37 +0900

    Title: 최대공약수 구하기, Time: 133ms, Memory: 10MB, Status: Passed - CodeTree

--- 240215/최대공약수 구하기/find-the-greatest-common-divisor.java
+++ 240215/최대공약수 구하기/find-the-greatest-common-divisor.java
@@ -13,7 +13,7 @@ public class Main {
     public static int getGcf(int n, int m) {
         int minN = n < m ? n : m;
         int maxN = Integer.MIN_VALUE;
-        for (int i=1; i<minN; i++) {
+        for (int i=1; i<=minN; i++) {
             if (n % i == 0 && m % i == 0) {
                 maxN = Math.max(maxN, i);
             }
