b18dfdd0a9a7230c4969392770b7118c60ef919f
bnjjong <44669620+bnjjong@users.noreply.github.com>
2024-02-21 23:37:42 +0900

    Title: 함수를 이용한 합과 소수 판별, Time: 133ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240221/함수를 이용한 합과 소수 판별/use-functions-to-determine-sums-and-decimals.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int cnt = 0;
+        for (int i=a; i<=b; i++) {
+            if (isConditionN(i)) {
+                cnt++;
+            }
+        }
+        System.out.print(cnt);
+    }
+
+    public static boolean isConditionN(int n) {
+        if (isPrime(n)) {
+            // System.out.println(n);
+            int sum = 0;
+            while(n >= 10) {
+                sum += n % 10;
+                n = n/10;
+                if (n < 10) {
+                    sum += n;
+                }
+            }
+            if (sum % 2 ==0) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean isPrime(int number) {
+        if (number <= 1) {
+            return false;
+        }
+        if (number <= 3) {
+            return true;
+        }
+        if (number % 2 == 0 || number % 3 == 0) {
+            return false;
+        }
+        for (int i = 5; i * i <= number; i += 6) {
+            if (number % i == 0 || number % (i + 2) == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
