4188eda2073d4d7c1d10b3fc10f3518b3cd91794
bnjjong <44669620+bnjjong@users.noreply.github.com>
2024-02-21 23:23:34 +0900

    Title: 함수를 이용한 온전수 판별, Time: 127ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240221/함수를 이용한 온전수 판별/determining-the-whole-number-using-a-function.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int cnt = 0;
+        for (int i=a; i<=b; i++) {
+            if (isIntactN(i)) {
+                cnt++;
+            }
+        }
+        System.out.print(cnt);
+    }
+    
+    public static boolean isIntactN(int n) {
+        if (n % 2 == 0 ||
+        n % 10 == 5 ||
+        (n % 3 == 0 && n % 9 !=0)) {
+            return false;
+        }
+        return true;
+    }
+}
