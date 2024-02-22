bacecf7eb8c369ffa17362cf5f6951a6f2aaad45
bnjjong <44669620+bnjjong@users.noreply.github.com>
2024-02-22 20:06:08 +0900

    Title: 함수를 이용한 연속부분수열 여부 판단하기, Time: 177ms, Memory: 10MB, Status: Passed - CodeTree

--- 240222/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.java
+++ 240222/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.java
@@ -15,23 +15,22 @@ public class Main {
             a2Arr[i] = sc.nextInt();
         }
 
-        System.out.print(isContains(a1Arr, a2Arr)? "Yes" : "No");
-
-
-        
+        System.out.print(isContains(a1Arr, a2Arr)? "Yes" : "No");        
     }
 
     public static boolean isContains(int[]a , int[] b) {
         int startN = b[0];
         for (int i=0; i<a.length; i++) {
             // 현재 인덱스에서 남은 인덱스 차를 구하고 비교할 b와 사이즈 비교해서 break
-            // 1 2 1 3
-            // 1 3
+            // 4자리수에서 현재 인덱스가 2라면 
+            // 4-2 = 2 즉 2자리가 남게 되고 b array 사이즈가 3이라면 굳이 비교할 필요가 없다.
+            // 따라서 아래와 같은 조건일때는 break 처리 하고 루프를 빠져 나간다.
             if (a.length - i < b.length) {
                 break;
             } 
             if (a[i] == startN) {
                 boolean isMatch = true;
+                // a 배열과 b 배열을 순차적으로 비교 한다.
                 for (int j=0; j<b.length; j++) {
                     if (a[i+j] != b[j]) {
                         isMatch = false;
