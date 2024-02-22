46d17a8bcda91444638893beca05a00fa1084db3
bnjjong <44669620+bnjjong@users.noreply.github.com>
2024-02-22 19:58:37 +0900

    Title: 함수를 이용한 연속부분수열 여부 판단하기, Time: 133ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240222/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int a1 = sc.nextInt();
+        int a2 = sc.nextInt();
+
+        int[] a1Arr = new int[a1];
+        for (int i=0; i<a1; i++) {
+            a1Arr[i] = sc.nextInt();
+        }
+        int[] a2Arr = new int[a2];
+        for (int i=0; i<a2; i++) {
+            a2Arr[i] = sc.nextInt();
+        }
+
+        System.out.print(isContains(a1Arr, a2Arr)? "Yes" : "No");
+
+
+        
+    }
+
+    public static boolean isContains(int[]a , int[] b) {
+        int startN = b[0];
+        for (int i=0; i<a.length; i++) {
+            // 현재 인덱스에서 남은 인덱스 차를 구하고 비교할 b와 사이즈 비교해서 break
+            if ((a.length - i + 1) < b.length) {
+                break;
+            } 
+            if (a[i] == startN) {
+                boolean isMatch = true;
+                for (int j=0; j<b.length; j++) {
+                    if (a[i+j] != b[j]) {
+                        isMatch = false;
+                    }
+                }
+                return isMatch;
+            }
+        }
+        return false;
+    }
+}
