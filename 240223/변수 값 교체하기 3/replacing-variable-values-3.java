58c8271f50abb4bc3aeb9135f4a824babfc36d43
bnjjong <44669620+bnjjong@users.noreply.github.com>
2024-02-23 14:57:59 +0900

    Title: 변수 값 교체하기 3, Time: 95ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240223/변수 값 교체하기 3/replacing-variable-values-3.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        int a = 3;
+        int b = 5;
+
+        // swap
+        int x = a;
+        a = b;
+        b = x;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
