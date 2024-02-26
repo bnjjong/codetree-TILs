058476b27646d4e2835217bc2b7ba3315f94c7ad
bnjjong <44669620+bnjjong@users.noreply.github.com>
2024-02-26 23:48:32 +0900

    Title: 2개 이상의 알파벳, Time: 116ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240226/2개 이상의 알파벳/more-than-one-alphabet.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        System.out.print(isMultipleChar(a) ? "Yes":"No");
+
+    }
+
+    public static boolean isMultipleChar(String a) {
+        char[] ac = a.toCharArray();
+        
+        Set<Character> charSet = new HashSet<>();
+        for (int i=0; i<ac.length; i++) {
+            if (charSet.contains(ac[i])) {
+                if (charSet.size() > 2 ) {
+                    return true;
+                }
+            }
+            charSet.add(ac[i]);
+        }
+
+        return false;
+    }
+}
