b1fe5ffcb45ce287b8b6098e0b73d4d9488562e1
bnjjong <44669620+bnjjong@users.noreply.github.com>
2024-02-28 17:45:59 +0900

    Title: 2개 이상의 알파벳, Time: 149ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240228/2개 이상의 알파벳/more-than-one-alphabet.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        System.out.print(isMultipleChar(a) ? "Yes":"No");
+
+    }
+
+    public static boolean isMultipleChar(String a) {
+        char[] ac = a.toCharArray();
+        
+        Set<Character> charSet = new HashSet<>();
+        for (int i=0; i<ac.length; i++) {
+            charSet.add(ac[i]);
+            if (charSet.size() >= 2 ) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
