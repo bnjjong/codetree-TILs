45175fda951abcdcf52fa7740761adcacddfc948
bnjjong <44669620+bnjjong@users.noreply.github.com>
2024-03-04 13:47:58 +0900

    Title: 재귀함수를 이용한 최댓값, Time: 150ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240304/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] numArr = new int[n];
+        for (int i=0; i<n; i++) {
+            numArr[i] = sc.nextInt();
+        }
+
+        System.out.println(getMaxN(n-1, numArr));
+
+        
+
+
+    }
+
+    public static int getMaxN(int n, int[] arr) {
+        if (n == 1) {
+            return arr[n];
+        }
+
+        return Math.max(arr[n], getMaxN(n-1, arr));
+
+    }
+}
