b809425f03e684f292635e841f115e6407b965c1
bnjjong <44669620+bnjjong@users.noreply.github.com>
2024-03-21 15:25:01 +0900

    Title: 사는 지역, Time: 154ms, Memory: 10MB, Status: Passed - CodeTree

--- 240321/사는 지역/where-live.java
+++ 240321/사는 지역/where-live.java
@@ -20,7 +20,7 @@ public class Main {
         
 
         Address[] arr = new Address[n];
-        String[] names = new String[n];
+        // String[] names = new String[n];
         for (int i=0; i<n; i++) {
             String name = sc.next();
             String postcode = sc.next();
@@ -30,20 +30,33 @@ public class Main {
                 postcode,
                 city
             );
-            names[i] = name;
+            // names[i] = name;
         }
 
-        Arrays.sort(names, Collections.reverseOrder());
-        String lastIdxName = names[0];
-        
-        for (int i=0; i<n; i++) {
-            if (lastIdxName == arr[i].name) {
-                System.out.println("name " + arr[i].name);
-                System.out.println("addr " + arr[i].postcode);
-                System.out.println("city " + arr[i].city);
-                break;
-            }
+        int lastIdx = 0;
+        for(int i = 1; i < n; i++) {
+            // 기준 값과 비교대상이 동일한 값일 경우 0
+            // 기준 값이 비교대상 보다 작은 경우 -1
+            // 기준 값이 비교대상 보다 큰 경우 1 <<<<<<<<<
+            if(arr[i].name.compareTo(arr[lastIdx].name) > 0)
+                lastIdx = i;
         }
+
+        System.out.println("name " + arr[lastIdx].name);
+        System.out.println("addr " + arr[lastIdx].postcode);
+        System.out.println("city " + arr[lastIdx].city);
+
+        // Arrays.sort(names, Collections.reverseOrder());
+        // String lastIdxName = names[0];
+        
+        // for (int i=0; i<n; i++) {
+        //     if (lastIdxName == arr[i].name) {
+        //         System.out.println("name " + arr[i].name);
+        //         System.out.println("addr " + arr[i].postcode);
+        //         System.out.println("city " + arr[i].city);
+        //         break;
+        //     }
+        // }
         
     }
 }
