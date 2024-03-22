d0c1f866e85bd145dea4f70c9a1fe143dc3e9cc0
bnjjong <44669620+bnjjong@users.noreply.github.com>
2024-03-22 22:03:38 +0900

    Title: 줄 세우기, Time: 376ms, Memory: 17MB, Status: Passed - CodeTree

--- 240322/줄 세우기/line-up-students.java
+++ 240322/줄 세우기/line-up-students.java
@@ -14,13 +14,21 @@ class Student implements Comparable<Student>{
         // 키가 더 큰 학생이 앞에 와야 합니다.
         // 키가 동일하다면, 몸무게가 더 큰 학생이 앞에 와야 합니다.
         // 키와 몸무게가 동일하다면, 번호가 작은 학생이 앞에 와야 합니다.
-        if (this.height == student.height && this.weight == student.weight) {
-            return this.no - student.no;
-        }
-        else if (this.height == student.height) {
-            return student.weight - this.weight;
-        }
-        return student.height - this.height;
+        // if (this.height == student.height && this.weight == student.weight) {
+        //     return this.no - student.no;
+        // }
+        // else if (this.height == student.height) {
+        //     return student.weight - this.weight;
+        // }
+        // return student.height - this.height;
+        // 키가 크면 정렬 했을 때 앞에 와야 합니다.
+        if(this.height != student.height)
+            return student.height - this.height; // 내림 차순 정렬
+        // 몸무게가 크면 정렬 했을 때 앞에 와야합니다.
+        if(this.weight != student.weight)
+            return student.weight - this.weight; // 내림 차순
+        // 번호가 작으면 앞에 와야 합니다.
+        return this.no - student.no; // 그외 순번 오름 차순
         
     }
 }
