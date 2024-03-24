9dff92ee2906f612a16e598de857bc68984b4082
bnjjong <44669620+bnjjong@users.noreply.github.com>
2024-03-24 22:06:13 +0900

    Title: 원점으로부터의 거리, Time: 433ms, Memory: 18MB, Status: Passed - CodeTree

---
+++ 240324/원점으로부터의 거리/distance-from-origin.java
@@ -0,0 +1,42 @@
+import java.util.*;
+class Manhattan {
+    int no;
+    int x1;
+    int x2;
+    int distance;
+
+    public Manhattan(int no, int x1, int x2) {
+        this.no = no;
+        this.x1 = x1;
+        this.x2 = x2;
+
+        this.distance = Math.abs(x1) + Math.abs(x2);
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        Manhattan[] mArr = new Manhattan[n];
+        for (int i=0; i<n; i++) {
+            int x1 = sc.nextInt();
+            int x2 = sc.nextInt();
+            mArr[i] = new Manhattan(i+1, x1, x2);
+        }
+
+        Arrays.sort(mArr, (a,b) -> {
+            if (a.distance == b.distance) {
+                return a.no - b.no;
+            }
+            return a.distance - b.distance;
+            
+        });
+
+        for (int i=0; i<n; i++) {
+            System.out.println(mArr[i].no);
+        }
+
+    }
+}
