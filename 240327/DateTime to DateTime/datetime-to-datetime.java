dc7a68249702d7557da86432cb84a71f2da796f1
bnjjong <44669620+bnjjong@users.noreply.github.com>
2024-03-27 00:10:54 +0900

    Title: DateTime to DateTime, Time: 130ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240327/DateTime to DateTime/datetime-to-datetime.java
@@ -0,0 +1,47 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int d = sc.nextInt();
+        int h = sc.nextInt();
+        int m = sc.nextInt();
+
+        int sd = 11;
+        int sh = 11;
+        int sm = 11;
+
+        int totalMin1 = (d * 24 * 60) + (h * 60) + m;
+        int totalMin2 = (sd * 24 * 60) + (sh * 60) + sm;
+        
+        // 11일 11시 11분 보다 작다면 (계산 불가)
+        if (totalMin1 < totalMin2) {
+            System.out.println(-1);
+        } else {
+            int elapsedMinute = 0;
+            while(true) {
+                if (sd == d && sh == h && sm == m) {
+                    break;
+                }
+                elapsedMinute++;
+                sm++;
+                // System.out.printf("%d-%d:%d\n",sd, sh, sm);
+                if (sm == 60) {
+                    sh++;
+                    sm = 0;
+
+                    if (sh == 24) {
+                        sd++;
+                        sh = 0;
+                    }
+                }
+            }
+            System.out.println(elapsedMinute);
+
+        }
+
+        
+
+        
+    }
+}
