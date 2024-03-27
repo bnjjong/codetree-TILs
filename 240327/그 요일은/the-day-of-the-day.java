b61ab80d7fa0456335a362192e476db82e84f088
bnjjong <44669620+bnjjong@users.noreply.github.com>
2024-03-27 21:11:22 +0900

    Title: 그 요일은, Time: 149ms, Memory: 10MB, Status: Passed - CodeTree

--- 240327/그 요일은/the-day-of-the-day.java
+++ 240327/그 요일은/the-day-of-the-day.java
@@ -2,7 +2,7 @@ import java.util.*;
 public class Main {
     public static int numOfDays(int m, int d) {
         // 계산 편의를 위해 월마다 몇 일이 있는지를 적어줍니다. 
-        int[] days = new int[]{0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        int[] days = new int[]{0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
         int totalDays = 0;
         
         // 1월부터 (m - 1)월 까지는 전부 꽉 채워져 있습니다.
