412840b0e70ba08fbd36d68a00bf1ea111763231
bnjjong <44669620+bnjjong@users.noreply.github.com>
2024-04-12 22:01:45 +0900

    Title: 두 사람, Time: 119ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240412/두 사람/two-person.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int age1 = sc.nextInt();
+        String gender1 = sc.next();
+
+        int age2 = sc.nextInt();
+        String gender2 = sc.next();
+
+        if (age1 >= 19 && gender1.equals("M") || age2 >= 19 && gender2.equals("M")) {
+            System.out.println(1);
+        } else {
+            System.out.println(0);
+        }
+
+        
+    }
+}
