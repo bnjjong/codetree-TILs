b015cde9c07f003f5a58751dbc2739772d408971
bnjjong <44669620+bnjjong@users.noreply.github.com>
2024-04-19 14:48:16 +0900

    Title: 증가시키며 출력하기 2, Time: 89ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240419/증가시키며 출력하기 2/increase-and-print-2.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        int idx = 10;
+        while(true) {
+            if (idx > 26) {
+                break;
+            }
+            System.out.print(idx++ + " ");
+            
+        }
+    }
+}
