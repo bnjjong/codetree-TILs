c51e954e6ab4aa0ad96f98b56e0463246cea8e0c
bnjjong <44669620+bnjjong@users.noreply.github.com>
2024-04-23 21:52:03 +0900

    Title: a/b 출력, Time: 147ms, Memory: 10MB, Status: Passed - CodeTree

--- 240423/a/b 출력/a-divide-b.java
+++ 240423/a/b 출력/a-divide-b.java
@@ -6,13 +6,28 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
-        BigDecimal dividend = new BigDecimal(a);
-        BigDecimal divisor = new BigDecimal(b);
+        // BigDecimal dividend = new BigDecimal(a);
+        // BigDecimal divisor = new BigDecimal(b);
 
-        // 나눗셈 수행
-        BigDecimal result = dividend.divide(divisor, 20, BigDecimal.ROUND_DOWN);
+        // // 나눗셈 수행
+        // BigDecimal result = dividend.divide(divisor, 20, BigDecimal.ROUND_DOWN);
 
-        System.out.println(result);
+        // System.out.println(result);
+        // 정수 부분을 먼저 출력합니다.
+        System.out.print(a / b + ".");
+        
+        // a를 b로 나눈 나머지를 시작으로
+        // 소수점 자리를 하나씩 계산합니다.
+        a %= b;
+        for(int i = 0; i < 20; i++) {
+            // 나머지에 10 곱한 값을 기준으로
+            // b로 나누었을 떄의 몫을 구해줍니다.
+            a *= 10;
+            System.out.print(a / b);
+            
+            // a를 b로 나눈 나머지를 게속 갱신해줍니다.
+            a %= b;
+        }
         
         
 
