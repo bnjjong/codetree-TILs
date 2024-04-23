83ce4dc8c83f63bb3199d64f9cb165c77f03c096
bnjjong <44669620+bnjjong@users.noreply.github.com>
2024-04-23 21:17:33 +0900

    Title: 문자 출력하기, Time: 125ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240423/문자 출력하기/print-text.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        String n = sc.next();
+        int idx = 1;
+        int endIdx = 8;
+        while(idx++ <= endIdx) {
+            System.out.print(n);
+            
+        }
+    }
+}
