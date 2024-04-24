9e81b7fcead3b204189f76c5000828828c567b52
bnjjong <44669620+bnjjong@users.noreply.github.com>
2024-04-24 14:24:38 +0900

    Title: 숫자의 증감, Time: 129ms, Memory: 10MB, Status: Passed - CodeTree

--- 240424/숫자의 증감/increasing-and-decreasing-numbers.java
+++ 240424/숫자의 증감/increasing-and-decreasing-numbers.java
@@ -6,8 +6,7 @@ public class Main {
         String c = sc.next();
         int n = sc.nextInt();
         int idx;
-
-        boolean isPlus =  ? true : false;
+        boolean isPlus;
 
         if (c.equals("A")) {
             isPlus = true;
