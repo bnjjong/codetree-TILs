bb4ebd767ce01b2863a617788c543253c00ce9ec
bnjjong <44669620+bnjjong@users.noreply.github.com>
2024-04-26 15:49:26 +0900

    Title: 369 게임, Time: 126ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240426/369 게임/369-game.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        
+        for (int i=1; i<=n; i++) {
+            String iStr = String.valueOf(i);
+            if (iStr.contains("3") || iStr.contains("6") || iStr.contains("9")) {
+                System.out.print(0+" ");
+            } else {
+                System.out.print(i+" ");
+            }
+        }
+    }
+}
