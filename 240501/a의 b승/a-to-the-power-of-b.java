b061a2e0362b1828a6eeb5972009bd966b1951f2
bnjjong <44669620+bnjjong@users.noreply.github.com>
2024-05-01 12:19:23 +0900

    Title: a의 b승, Time: 158ms, Memory: 10MB, Status: Passed - CodeTree

--- 240501/a의 b승/a-to-the-power-of-b.java
+++ 240501/a의 b승/a-to-the-power-of-b.java
@@ -3,9 +3,12 @@ public class Main {
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
+
+        
         int a = sc.nextInt();
         int b = sc.nextInt();
 
+        // a의 b승 구하기.
         int multiply = 1;
         for (int i=0; i<b; i++) {
             multiply *= a;
