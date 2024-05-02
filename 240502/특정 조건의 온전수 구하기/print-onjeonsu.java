61bee89f031a4e2ffbed2d368e32e240f63a9995
bnjjong <44669620+bnjjong@users.noreply.github.com>
2024-05-02 16:22:08 +0900

    Title: 특정 조건의 온전수 구하기, Time: 124ms, Memory: 10MB, Status: Passed - CodeTree

--- 240502/특정 조건의 온전수 구하기/print-onjeonsu.java
+++ 240502/특정 조건의 온전수 구하기/print-onjeonsu.java
@@ -6,6 +6,9 @@ public class Main {
         int n = sc.nextInt();
 
         for (int i=1; i<=n; i++) {
+            // 2로 나누어 떨어지는 경우
+            // 일의 자리가 5인 경우
+            // 3으로 나누어 떨어지면서 9로는 나누어 떨어지지 않는 수
             if (i % 2 == 0 || getOnePlace(i) == 5 || (i % 3 == 0 && i % 9 != 0) )  {
                 continue;
             }
