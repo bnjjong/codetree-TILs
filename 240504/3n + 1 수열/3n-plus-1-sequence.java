37e50c9cd586346e93ced17c383f58eb2a3b2ced
bnjjong <44669620+bnjjong@users.noreply.github.com>
2024-05-04 22:45:57 +0900

    Title: 3n + 1 수열, Time: 118ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240504/3n + 1 수열/3n-plus-1-sequence.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int cnt = 0;
+        while(true) {
+            if (n == 1) {
+                System.out.print(cnt);
+                break;
+            }
+            
+            if (n % 2 == 0) {
+                n /= 2;
+            } else {
+                n *= 3;
+                n++;
+            }
+
+            cnt++;
+        }
+    }
+}
