6d0f7baa6f3303a59c5afee276ac951220452280
bnjjong <44669620+bnjjong@users.noreply.github.com>
2024-05-04 23:07:11 +0900

    Title: 조건에 따른 연산, Time: 146ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240504/조건에 따른 연산/operatino-by-rule.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int cnt = 0;
+        while (true) {
+            if (n >= 1000) {
+                break;
+            }
+            if (n % 2 ==0) {
+                n *= 3;
+                n++;
+            } else {
+                n *= 2;
+                n += 2;
+            }
+            cnt ++;
+        }
+        System.out.println(cnt);
+    }
+}
