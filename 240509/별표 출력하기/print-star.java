e14c432c930503d1c1476d769782d26322e2b1a7
bnjjong <44669620+bnjjong@users.noreply.github.com>
2024-05-09 14:10:01 +0900

    Title: 별표 출력하기, Time: 134ms, Memory: 10MB, Status: Passed - CodeTree

--- 240509/별표 출력하기/print-star.java
+++ 240509/별표 출력하기/print-star.java
@@ -8,7 +8,7 @@ public class Main {
         
         for (int i=1; i<=n+(n-1); i++) {
             int jSize = i <= n ? i : n-(i-n);
-            for (int j=0; j<jSize; j++) {
+            for (int j=1; j<=jSize; j++) {
                 System.out.print("* ");
             }
             System.out.println();
