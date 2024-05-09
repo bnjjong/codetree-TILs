124f24eb6555572e52c7af540616d92e363ee6d9
bnjjong <44669620+bnjjong@users.noreply.github.com>
2024-05-09 14:19:14 +0900

    Title: 별표 출력하기 4, Time: 133ms, Memory: 10MB, Status: Passed - CodeTree

--- 240509/별표 출력하기 4/print-star-4.java
+++ 240509/별표 출력하기 4/print-star-4.java
@@ -12,5 +12,20 @@ public class Main {
             }
             System.out.println();
         }
+
+        // 가독성은 이게 더 높음.
+        // 길이가 n인 직각삼각형을 뒤집어 출력합니다.
+        // for(int i = n-1; i >= 0; i--) {
+        //     for(int j = 0; j <= i; j++)
+        //         System.out.print("* ");
+        //     System.out.println();
+        // }
+        
+        // // 길이가 n-1인 직각삼각형을 출력합니다.
+        // for(int i = 1; i < n; i++) {
+        //     for(int j = 0; j <= i; j++)
+        //         System.out.print("* ");
+        //     System.out.println();
+        // }
     }
 }
