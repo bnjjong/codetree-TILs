a9dacd45f01692b5d1c98a78a40fefccebf18ffa
bnjjong <44669620+bnjjong@users.noreply.github.com>
2024-05-10 23:39:49 +0900

    Title: 별표 출력하기 3, Time: 127ms, Memory: 10MB, Status: Passed - CodeTree

--- 240510/별표 출력하기 3/print-star-3.java
+++ 240510/별표 출력하기 3/print-star-3.java
@@ -9,10 +9,11 @@ public class Main {
             for (int j=0; j<i; j++) {
                 System.out.print("  ");
             }
-            for (int j=0; j<(2*n)-(2*i)-1; j++) { // 4
+            // idx 1번째 줄 = (2*5)-(2*0)-1 = 10-0-1 = 9;
+            // idx 2번째 줄 = (2*5)-(2*1)-1 = 10-2-1 = 7;
+            for (int j=0; j<(2*n)-(2*i)-1; j++) { 
                 System.out.print("* ");
-            }
-            
+            }           
             System.out.println();
         }
     }
