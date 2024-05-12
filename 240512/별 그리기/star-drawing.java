10a15df02427666fae84962fadce7b2460600c62
bnjjong <44669620+bnjjong@users.noreply.github.com>
2024-05-12 21:55:45 +0900

    Title: 별 그리기, Time: 324ms, Memory: 13MB, Status: Passed - CodeTree

--- 240512/별 그리기/star-drawing.java
+++ 240512/별 그리기/star-drawing.java
@@ -11,13 +11,13 @@ public class Main {
             // 1 v***   0 = 1,  3      3 i*2+1
             // 2 *****    = 0,  5.     5 i*2+1
 
-            // 3 v***   0 = 1          3 3-(2* (i%n)) 3+0 = 3
+            // 3 v***   0 = 1          3 n*2-3 4 - 0
             // 4 vv*    0 = 2          1 3-(2* (i%n)) 3-2 = 1
 
             // 3 + (2 * i)
             // 공백 찍기
             int spaceSize = i < n ? n-i-1 : i-n+1;
-            int starSize = i < n ? i*2+1 : 3-(2*(i%n));
+            int starSize = i < n ? i*2+1 :  (n*2-3)-(2*(i%n)); // 앞에 계산식은 시작점 구하기, 뒤에 마이너스의 경우 줄어드는 사이즈
             for (int j=0; j<spaceSize; j++) {
                 System.out.print(" ");
             }
