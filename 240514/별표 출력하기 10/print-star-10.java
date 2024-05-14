c760d803de646924a23cfb20eafe272045dd0a38
bnjjong <44669620+bnjjong@users.noreply.github.com>
2024-05-14 21:08:46 +0900

    Title: 별표 출력하기 10, Time: 138ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240514/별표 출력하기 10/print-star-10.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int i=0; i<n*2; i++) {
+            int idx = i % 2 == 0 ? 1 + (i / 2) : n - (i - 1) / 2;
+            for (int j=0; j<idx; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
