0052569438c77e33fe4c1726d9727c23a71677e1
bnjjong <44669620+bnjjong@users.noreply.github.com>
2024-05-15 23:38:19 +0900

    Title: 별표 출력하기 11, Time: 126ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240515/별표 출력하기 11/print-star-11.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        // i, n = 3
+        // 0 * * * * * * * - 2의 배수 일때 전체 찍고
+        // 1 *   *   *   * - 홀수 일때는 별+공백+별
+        // 2 * * * * * * *
+        // 3 *   *   *   *
+        // 4 * * * * * * *
+        // 5 *   *   *   *
+        // 6 * * * * * * *
+        int size = n+(n*(n-1))-(n-1); // 3+(3*2)-2 = 7
+        for (int i=0; i<size; i++) { 
+            for (int j=0; j<size; j++) {
+                if (i % 2 == 0) {
+                    System.out.print("* ");
+                } else {
+                  if (j % 2 == 0) {
+                    System.out.print("* ");
+                  } else {
+                    System.out.print("  ");
+                  }
+                }
+            }
+            System.out.println();
+            
+        }
+    }
+}
