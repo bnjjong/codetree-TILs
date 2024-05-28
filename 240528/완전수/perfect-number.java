94c4a2fa5a8a9a8dc9a76939828be31eeceb7e01
bnjjong <44669620+bnjjong@users.noreply.github.com>
2024-05-28 15:37:20 +0900

    Title: 완전수, Time: 151ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240528/완전수/perfect-number.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int s = sc.nextInt();
+        int e = sc.nextInt();
+
+        int cnt = 0;
+        for (int i=s; i<=e; i++) {
+            int sum = 0;
+            for (int j=1; j<=i/2; j++) {
+                if (i % j == 0) {
+                    sum += j;       
+                }
+            }
+            
+            if (sum == i) {
+                cnt++;
+            }
+        }
+        System.out.println(cnt);
+    }
+}
