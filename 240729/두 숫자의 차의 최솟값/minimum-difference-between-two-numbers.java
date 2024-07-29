52ecb99d9aa79f3b63b56ed239c49f2cc8a66954
bnjjong <44669620+bnjjong@users.noreply.github.com>
2024-07-29 22:52:53 +0900

    Title: 두 숫자의 차의 최솟값, Time: 142ms, Memory: 10MB, Status: Passed - CodeTree

--- 240729/두 숫자의 차의 최솟값/minimum-difference-between-two-numbers.java
+++ 240729/두 숫자의 차의 최솟값/minimum-difference-between-two-numbers.java
@@ -10,20 +10,31 @@ public class Main {
             nArr[i] = sc.nextInt();
         }
 
-        int min = Integer.MAX_VALUE;
-        for (int i=0; i<n; i++) {
-            int compareN1 = nArr[i];
-            for (int j=0; j<n; j++) {
-                if (i == j) {
-                    continue;
-                }
-                int compareN2 = nArr[j];
-                int diff = Math.abs(compareN1-compareN2);
-                if (min > diff) {
-                    min = diff;
-                }
+        // int min = Integer.MAX_VALUE;
+        // for (int i=0; i<n; i++) {
+        //     int compareN1 = nArr[i];
+        //     for (int j=0; j<n; j++) {
+        //         if (i == j) {
+        //             continue;
+        //         }
+        //         int compareN2 = nArr[j];
+        //         int diff = Math.abs(compareN1-compareN2);
+        //         if (min > diff) {
+        //             min = diff;
+        //         }
+        //     }
+        // }
+        // System.out.println(min);
+        
+        // O(N)
+        int min = nArr[1]-nArr[0];
+
+        for (int i=2; i<n; i++) {
+            if (min > nArr[i]- nArr[i-1]) {
+                min = nArr[i]- nArr[i-1];
             }
         }
+
         System.out.println(min);
     }
 }
