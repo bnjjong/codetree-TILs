4895bb2cafcb018756e1c1e6982b3c4df6c8e390
bnjjong <44669620+bnjjong@users.noreply.github.com>
2024-07-29 22:50:22 +0900

    Title: 두 숫자의 차의 최솟값, Time: 133ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240729/두 숫자의 차의 최솟값/minimum-difference-between-two-numbers.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int[] nArr = new int[n];
+        for (int i=0; i<n; i++) {
+            nArr[i] = sc.nextInt();
+        }
+
+        int min = Integer.MAX_VALUE;
+        for (int i=0; i<n; i++) {
+            int compareN1 = nArr[i];
+            for (int j=0; j<n; j++) {
+                if (i == j) {
+                    continue;
+                }
+                int compareN2 = nArr[j];
+                int diff = Math.abs(compareN1-compareN2);
+                if (min > diff) {
+                    min = diff;
+                }
+            }
+        }
+        System.out.println(min);
+    }
+}
