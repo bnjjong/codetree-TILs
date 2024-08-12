046cc3f25d561217bda8258a6de1f09681006e80
bnjjong <44669620+bnjjong@users.noreply.github.com>
2024-08-12 13:54:18 +0900

    Title: 지그재그로 숫자 채우기, Time: 130ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240812/지그재그로 숫자 채우기/zigzag-numbering.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        
+        for (int i=0; i<n; i++) {
+            for (int j=0; j<m; j++) {
+                int idx = 0;
+                if (j % 2 == 0) {
+                    idx = i+(j * n);
+                } else {
+                    idx = (j * 2 * n)- 1 - i;
+                }
+                System.out.print(idx+" ");
+            }
+            System.out.println();
+        }
+    }
+}
