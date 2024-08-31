9a71e1e7aa1266b0bbb7f96f18b53cb4329ab6d1
bnjjong <44669620+bnjjong@users.noreply.github.com>
2024-09-01 04:09:32 +0900

    Title: 더 긴 문자열, Time: 129ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240901/더 긴 문자열/longer-string.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        String b = sc.next();
+        // String a = str.split(" ")[0];
+        // String b = str.split(" ")[1];
+
+        if (a.length() > b.length()) {
+            System.out.printf("%s %d", a, a.length());
+        } else if (a.length() < b.length()) {
+            System.out.printf("%s %d", b, b.length());
+        } else {
+            System.out.println("same");
+        }
+
+        
+    }
+}
