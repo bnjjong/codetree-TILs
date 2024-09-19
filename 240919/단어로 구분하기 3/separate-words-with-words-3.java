d8c4801d6c38effdaee9cabfc4ff50e254e49cb3
bnjjong <44669620+bnjjong@users.noreply.github.com>
2024-09-19 23:33:32 +0900

    Title: 단어로 구분하기 3, Time: 117ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240919/단어로 구분하기 3/separate-words-with-words-3.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        String[] arr = sc.nextLine().split(" ");
+
+        
+        for (int i=arr.length-1; i>=0; i--) {
+            System.out.println(arr[i]);
+        }
+    }
+}
